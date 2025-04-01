fb19a9caac858e1b984399202b01126c2556d716
sunaoka <sunaoka@users.noreply.github.com>
2025-04-01 20:45:04 +0000

    Update Structures from AWS SDK for PHP 3.342.18
    <https://github.com/aws/aws-sdk-php/releases/tag/3.342.18>

--- structures/CleanRooms/UpdateCollaboration/UpdateCollaborationRequest.php
+++ structures/CleanRooms/UpdateCollaboration/UpdateCollaborationRequest.php
@@ -8,6 +8,7 @@ use Sunaoka\Aws\Structures\Request;
  * @property string $collaborationIdentifier
  * @property string|null $name
  * @property string|null $description
+ * @property 'SPARK'|'CLEAN_ROOMS_SQL'|null $analyticsEngine
  */
 class UpdateCollaborationRequest extends Request
 {
@@ -15,7 +16,8 @@ class UpdateCollaborationRequest extends Request
      * @param array{
      *     collaborationIdentifier: string,
      *     name?: string|null,
-     *     description?: string|null
+     *     description?: string|null,
+     *     analyticsEngine?: 'SPARK'|'CLEAN_ROOMS_SQL'|null
      * } $args
      */
     public function __construct(array $args)
--- structures/SageMaker/CreateNotebookInstanceLifecycleConfig/CreateNotebookInstanceLifecycleConfigRequest.php
+++ structures/SageMaker/CreateNotebookInstanceLifecycleConfig/CreateNotebookInstanceLifecycleConfigRequest.php
@@ -8,6 +8,7 @@ use Sunaoka\Aws\Structures\Request;
  * @property string $NotebookInstanceLifecycleConfigName
  * @property list<Shapes\NotebookInstanceLifecycleHook>|null $OnCreate
  * @property list<Shapes\NotebookInstanceLifecycleHook>|null $OnStart
+ * @property list<Shapes\Tag>|null $Tags
  */
 class CreateNotebookInstanceLifecycleConfigRequest extends Request
 {
@@ -15,7 +16,8 @@ class CreateNotebookInstanceLifecycleConfigRequest extends Request
      * @param array{
      *     NotebookInstanceLifecycleConfigName: string,
      *     OnCreate?: list<Shapes\NotebookInstanceLifecycleHook>|null,
-     *     OnStart?: list<Shapes\NotebookInstanceLifecycleHook>|null
+     *     OnStart?: list<Shapes\NotebookInstanceLifecycleHook>|null,
+     *     Tags?: list<Shapes\Tag>|null
      * } $args
      */
     public function __construct(array $args)
