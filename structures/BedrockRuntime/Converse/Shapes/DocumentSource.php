70768b27dd4baa9c163527f992d1869eed2f92ce
sunaoka <sunaoka@users.noreply.github.com>
2025-04-25 20:45:27 +0000

    Update Structures from AWS SDK for PHP 3.342.35
    <https://github.com/aws/aws-sdk-php/releases/tag/3.342.35>

--- structures/BedrockRuntime/Converse/Shapes/DocumentSource.php
+++ structures/BedrockRuntime/Converse/Shapes/DocumentSource.php
@@ -6,11 +6,15 @@ use Sunaoka\Aws\Structures\Shape;
 
 /**
  * @property string|resource|\Psr\Http\Message\StreamInterface|null $bytes
+ * @property S3Location|null $s3Location
  */
 class DocumentSource extends Shape
 {
     /**
-     * @param array{bytes?: string|resource|\Psr\Http\Message\StreamInterface|null} $args
+     * @param array{
+     *     bytes?: string|resource|\Psr\Http\Message\StreamInterface|null,
+     *     s3Location?: S3Location|null
+     * } $args
      */
     public function __construct(array $args = [])
     {
