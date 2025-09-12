c566ed3421116bca03244b73157a6c031ac44194
sunaoka <sunaoka@users.noreply.github.com>
2025-09-12 20:42:32 +0000

    Update Structures from AWS SDK for PHP 3.356.17
    <https://github.com/aws/aws-sdk-php/releases/tag/3.356.17>

--- structures/PaymentCryptography/ExportKey/Shapes/ExportTr34KeyBlock.php
+++ structures/PaymentCryptography/ExportKey/Shapes/ExportTr34KeyBlock.php
@@ -7,7 +7,9 @@ use Sunaoka\Aws\Structures\Shape;
 /**
  * @property string $CertificateAuthorityPublicKeyIdentifier
  * @property string $WrappingKeyCertificate
- * @property string $ExportToken
+ * @property string|null $ExportToken
+ * @property string|null $SigningKeyIdentifier
+ * @property string|null $SigningKeyCertificate
  * @property 'X9_TR34_2012' $KeyBlockFormat
  * @property string|null $RandomNonce
  * @property KeyBlockHeaders|null $KeyBlockHeaders
@@ -18,7 +20,9 @@ class ExportTr34KeyBlock extends Shape
      * @param array{
      *     CertificateAuthorityPublicKeyIdentifier: string,
      *     WrappingKeyCertificate: string,
-     *     ExportToken: string,
+     *     ExportToken?: string|null,
+     *     SigningKeyIdentifier?: string|null,
+     *     SigningKeyCertificate?: string|null,
      *     KeyBlockFormat: 'X9_TR34_2012',
      *     RandomNonce?: string|null,
      *     KeyBlockHeaders?: KeyBlockHeaders|null
--- structures/PaymentCryptography/ImportKey/Shapes/ImportTr34KeyBlock.php
+++ structures/PaymentCryptography/ImportKey/Shapes/ImportTr34KeyBlock.php
@@ -7,7 +7,9 @@ use Sunaoka\Aws\Structures\Shape;
 /**
  * @property string $CertificateAuthorityPublicKeyIdentifier
  * @property string $SigningKeyCertificate
- * @property string $ImportToken
+ * @property string|null $ImportToken
+ * @property string|null $WrappingKeyIdentifier
+ * @property string|null $WrappingKeyCertificate
  * @property string $WrappedKeyBlock
  * @property 'X9_TR34_2012' $KeyBlockFormat
  * @property string|null $RandomNonce
@@ -18,7 +20,9 @@ class ImportTr34KeyBlock extends Shape
      * @param array{
      *     CertificateAuthorityPublicKeyIdentifier: string,
      *     SigningKeyCertificate: string,
-     *     ImportToken: string,
+     *     ImportToken?: string|null,
+     *     WrappingKeyIdentifier?: string|null,
+     *     WrappingKeyCertificate?: string|null,
      *     WrappedKeyBlock: string,
      *     KeyBlockFormat: 'X9_TR34_2012',
      *     RandomNonce?: string|null
